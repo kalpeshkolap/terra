4d85cb97d88077e23d44cebb7179f5574847dd92
kalpeshkolap <53437883+kalpeshkolap@users.noreply.github.com>
2022-01-13 18:57:49 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -9,8 +9,8 @@ terraform {
 #storing state file to remote location(aws_s3 backend)
 terraform {
   backend "s3" {
-    bucket = "********************"
-    key    = "***************"
+    bucket = "kalpeshterraformstate"
+    key    = "kalpeshterraformstate/terraform.tfstate"
     region = "us-west-2"
   }
 }
